203ef97ea6a7f5d766d177b1213e1f3c857c6917
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:19:42 -0500

    Update Root.java
    Final draft of Root.java

--- src/Root.java
+++ src/Root.java
@@ -1,14 +1,22 @@
-
+/**
+ * 
+ * @author Megan McKinney
+ * 
+ * The Root class implements the Calculator interface.
+ * This class is responsible for all calculations involved in taking the nth root.
+ *
+ */
 public class Root implements Calculator {
 	
 	/**
 	 * @param x double
 	 * @param y double
-	 * @returns the product of a root equation
+	 * @returns the solution with the nth root taken
+	 *
+	 * Handles the operations involved in taking the nth root.
 	 */
 	@Override
 	public double calculate(double x, double y) {
 		return Math.pow(Math.E, Math.log(x)/y);
 	}
-
 }
