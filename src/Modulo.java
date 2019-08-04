ae2ca4eb953ebb42b865de16f555564f7074fd18
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:12:14 -0500

    Update Modulo.java
    Final draft of Modulo.java.

--- src/Modulo.java
+++ src/Modulo.java
@@ -1,9 +1,22 @@
-
-public class Modulo implements Calculator{
+/**
+ * 
+ * @author mmcki
+ * 
+ * The Modulo class implements the Calculator interface.
+ * It is responsible for handling the modulo division in the calculator.
+ *
+ */
+public class Modulo implements Calculator {
 	
+	/**
+	 * @param x double is the first input value
+	 * @param y double is the second input value
+	 * @return remainder of the division
+	 * 
+	 * This method is responsible for handling the modulo division operation
+	 */
 	@Override
 	public double calculate(double x, double y) {
 		return x % y;
 	}
-
 }
