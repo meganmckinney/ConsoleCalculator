56c4ebc322da07fbf7a24138bddfe4b7fb2e665e
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:04:41 -0500

    Update ConsoleCalc.java
    Final draft of ConsoleCalc.java.

--- src/ConsoleCalc.java
+++ src/ConsoleCalc.java
@@ -1,15 +1,15 @@
 /**
  * 
  * @author mmcki
+ *
  *Main class, holds the main method and is responsible for executing the calculator
  *
  */
 public class ConsoleCalc {
-
+	
 	public static void main(String[] args) {
 		
 		UserInputProcessor input = new UserInputProcessor();
 		input.inputHandler();
 	}
-
 }
