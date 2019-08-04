76fcc37b2c22d8188a06fab26463b4079b7c8b39
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:15:15 -0500

    Update RaisePower.java
    Final draft of RaisePower.java.

--- src/RaisePower.java
+++ src/RaisePower.java
@@ -1,11 +1,23 @@
-
+/**
+ * 
+ * @author Megan McKinney
+ *
+ * RaisePower implements the Calculator interface.
+ * This class handles all operations associated with raising a value to the nth power.
+ *
+ */
 public class RaisePower implements Calculator {
 	
+	/**
+	 * @param x double is the first input value
+	 * @param y double is the second input value
+	 * @return the solution raised to the nth power
+	 * 
+	 * Handles the operations involved with raising a value to the nth power
+	 */
 	@Override
 	public double calculate(double x, double y){
 		
 		return Math.pow(x, y);
-		
 	}
-
 }
