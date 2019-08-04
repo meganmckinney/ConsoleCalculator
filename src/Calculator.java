3272cca8f305a99a8fbe66c35de56fdfb11c000a
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:02:28 -0500

    Update Calculator.java
    Final draft of Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,12 @@
 
+/**
+ * 
+ * @author mmcki
+ * 
+ * This interface provides the model for each type of mathematical operation.
+ *
+ */
 public interface Calculator {
 	
 	double calculate(double x, double y);
-	double userSolution = 0;
-
 }
