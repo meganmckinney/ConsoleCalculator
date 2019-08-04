945908a97587114697683b3a5f756f3e2d8e5562
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:27:40 -0500

    Update Division.java
    Finishing touches.

--- src/Division.java
+++ src/Division.java
@@ -1,6 +1,6 @@
 /**
  * 
- * @author mmcki
+ * @author Megan McKinney
  * 
  * The Division class implements the calculator interface.
  * It takes care of the calculations for division.
@@ -13,7 +13,7 @@ public class Division implements Calculator{
 	 * @param y double is the second input value passed in
 	 * @returns the solution of the division
 	 *
-	 * This method handles the calculations involved in division
+	 * This method handles the calculations involved in division.
 	 */
 	@Override
 	public double calculate (double x, double y) {
