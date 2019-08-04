8b6484a3d473748d53ea181fe9fcb8e33715e571
meganmckinney <51385832+meganmckinney@users.noreply.github.com>
2019-08-03 19:13:46 -0500

    Update Multiplication.java
    Final draft of Multiplication.java.

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,22 +1,22 @@
 /**
  * 
- * @author mmcki
+ * @author Megan McKinney
  * 
  * The Multiplication class implements the Calculator interface.
  * It takes care of any of the calculations involved in multiplication
  *
  */
-public class Multiplication implements Calculator{
+public class Multiplication implements Calculator {
 	
 	/**
 	 * @param x double is the first input value
 	 * @param y double is the second input value
+	 * @return the solution of the multiplication
 	 * 
-	 * This calculate method takes care of the calculations for multiplication
+	 * This calculate method takes care of the calculations for multiplication.
 	 */
 	@Override
 	public double calculate(double x, double y) {
 		return x * y;
 	}
-
 }
